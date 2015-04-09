d562da1f6e9a5b3cf2ca701e3ea0cc1a674bcf31
Cognizant Mobility Practise <CognizantMobility@users.noreply.github.com>
2015-04-09 12:23:02 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends   ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
