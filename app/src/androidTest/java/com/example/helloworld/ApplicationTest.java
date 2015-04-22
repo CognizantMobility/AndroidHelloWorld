16f90b1aac53e930c121a72aeedfc1abcc5c6855
Cognizant Mobility Practise <CognizantMobility@users.noreply.github.com>
2015-04-22 13:00:54 +0530

    Update ApplicationTest.java

--- app/src/androidTest/java/com/example/helloworld/ApplicationTest.java
+++ app/src/androidTest/java/com/example/helloworld/ApplicationTest.java
@@ -12,6 +12,6 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     }
 
     public void testMyName(){
-        assertEquals("rakesh" ,MainActivity.myName());
+        assertEquals("ramesh" ,MainActivity.myName());
     }
 }
